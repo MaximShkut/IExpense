d61d0bcb05dc436c7e0e4ab1767685d23ee6f2fa
MaximShkut <m.shkut344@gmail.com>
2023-08-04 11:52:10 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by user236450 on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
