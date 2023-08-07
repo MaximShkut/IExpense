46b5e20c5d5d672156183d114afe77ef60d5c4b9
MaximShkut <m.shkut344@gmail.com>
2023-08-07 11:12:31 +0200

    new project

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,36 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by user236450 on 8/7/23.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+class Expenses: ObservableObject {
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+
+        items = []
+    }
+    
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+}
